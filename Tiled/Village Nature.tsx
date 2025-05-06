6a5318319cc3b95ebfedcd57298ee95d282ef33b
Alexlayden1 <a_layden@outlook.com>
2025-05-06 09:55:45 -0230

    Collision added for terrain and some nature
    rock walls, and some tree stumps now have the rectangles for collision

---
+++ Tiled/Village Nature.tsx
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Village Nature" tilewidth="16" tileheight="16" tilecount="1188" columns="27">
+ <image source="../Tiles/Village/Tiles/Rural Village Nature Objects.png" width="432" height="704"/>
+ <tile id="55">
+  <objectgroup draworder="index" id="3">
+   <object id="6" x="2.004" y="8" width="12" height="5"/>
+   <object id="7" x="5" y="13" width="6" height="2"/>
+   <object id="8" x="0" y="0" width="16" height="8"/>
+   <object id="9" x="16" y="0" width="9" height="8"/>
+   <object id="10" x="-10" y="0" width="10" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="163">
+  <properties>
+   <property name="Collision" type="int" value="55"/>
+  </properties>
+ </tile>
+ <tile id="271">
+  <properties>
+   <property name="Collision" type="int" value="55"/>
+  </properties>
+ </tile>
+ <tile id="379">
+  <properties>
+   <property name="Collision" type="int" value="55"/>
+  </properties>
+ </tile>
+ <tile id="487">
+  <properties>
+   <property name="Collision" type="int" value="55"/>
+  </properties>
+ </tile>
+ <tile id="595">
+  <properties>
+   <property name="Collision" type="int" value="55"/>
+  </properties>
+ </tile>
+ <tile id="703">
+  <properties>
+   <property name="Collision" type="int" value="55"/>
+  </properties>
+ </tile>
+ <tile id="811">
+  <properties>
+   <property name="Collision" type="int" value="55"/>
+  </properties>
+ </tile>
+</tileset>
