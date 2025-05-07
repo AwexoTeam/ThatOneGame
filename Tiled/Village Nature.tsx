4a8745bc0330827f5110638dbe0344dd38cd916f
Alexlayden1 <a_layden@outlook.com>
2025-05-07 16:28:14 -0230

    Collision shapes caves + village nature
    Added the collision shapes for caves and the village nature tile packs

--- Tiled/Village Nature.tsx
+++ Tiled/Village Nature.tsx
@@ -1,6 +1,33 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="Village Nature" tilewidth="16" tileheight="16" tilecount="1188" columns="27">
  <image source="../Tiles/Village/Tiles/Rural Village Nature Objects.png" width="432" height="704"/>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="2.22045e-16" width="16" height="15">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="46">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5" y="0" width="11" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="47">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="10" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="Collision" type="int" value="46"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="Collision" type="int" value="47"/>
+  </properties>
+ </tile>
  <tile id="55">
   <objectgroup draworder="index" id="3">
    <object id="6" x="2.004" y="8" width="12" height="5"/>
@@ -10,9 +37,113 @@
    <object id="10" x="-10" y="0" width="10" height="7"/>
   </objectgroup>
  </tile>
+ <tile id="57">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="11" y="0" width="5" height="15"/>
+   <object id="2" x="2" y="0" width="9" height="9"/>
+  </objectgroup>
+ </tile>
+ <tile id="58">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="5" height="15"/>
+   <object id="2" x="5" y="0" width="10" height="9"/>
+  </objectgroup>
+ </tile>
+ <tile id="59">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="9" width="8" height="6"/>
+   <object id="2" x="0" y="0" width="16" height="9"/>
+  </objectgroup>
+ </tile>
+ <tile id="60">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="9" y="6" width="7" height="9"/>
+   <object id="2" x="1" y="0" width="15" height="6"/>
+  </objectgroup>
+ </tile>
+ <tile id="100">
+  <properties>
+   <property name="Collision" type="int" value="46"/>
+  </properties>
+ </tile>
+ <tile id="101">
+  <properties>
+   <property name="Collision" type="int" value="47"/>
+  </properties>
+ </tile>
+ <tile id="104">
+  <properties>
+   <property name="Collision" type="int" value="46"/>
+  </properties>
+ </tile>
+ <tile id="105">
+  <properties>
+   <property name="Collision" type="int" value="47"/>
+  </properties>
+ </tile>
+ <tile id="113">
+  <properties>
+   <property name="Collision" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="154">
+  <properties>
+   <property name="Collision" type="int" value="46"/>
+  </properties>
+ </tile>
+ <tile id="155">
+  <properties>
+   <property name="Collision" type="int" value="47"/>
+  </properties>
+ </tile>
+ <tile id="158">
+  <properties>
+   <property name="Collision" type="int" value="46"/>
+  </properties>
+ </tile>
+ <tile id="159">
+  <properties>
+   <property name="Collision" type="int" value="47"/>
+  </properties>
+ </tile>
  <tile id="163">
   <properties>
    <property name="Collision" type="int" value="55"/>
+  </properties>
+ </tile>
+ <tile id="165">
+  <properties>
+   <property name="Collision" type="int" value="57"/>
+  </properties>
+ </tile>
+ <tile id="166">
+  <properties>
+   <property name="Collision" type="int" value="58"/>
+  </properties>
+ </tile>
+ <tile id="167">
+  <properties>
+   <property name="Collision" type="int" value="59"/>
+  </properties>
+ </tile>
+ <tile id="168">
+  <properties>
+   <property name="Collision" type="int" value="59"/>
+  </properties>
+ </tile>
+ <tile id="208">
+  <properties>
+   <property name="Collision" type="int" value="46"/>
+  </properties>
+ </tile>
+ <tile id="209">
+  <properties>
+   <property name="Collision" type="int" value="47"/>
+  </properties>
+ </tile>
+ <tile id="221">
+  <properties>
+   <property name="Collision" type="int" value="5"/>
   </properties>
  </tile>
  <tile id="271">
@@ -20,9 +151,190 @@
    <property name="Collision" type="int" value="55"/>
   </properties>
  </tile>
+ <tile id="273">
+  <properties>
+   <property name="Collision" type="int" value="57"/>
+  </properties>
+ </tile>
+ <tile id="274">
+  <properties>
+   <property name="Collision" type="int" value="58"/>
+  </properties>
+ </tile>
+ <tile id="275">
+  <properties>
+   <property name="Collision" type="int" value="59"/>
+  </properties>
+ </tile>
+ <tile id="276">
+  <properties>
+   <property name="Collision" type="int" value="60"/>
+  </properties>
+ </tile>
+ <tile id="277">
+  <properties>
+   <property name="Collision" type="int" value="61"/>
+  </properties>
+ </tile>
+ <tile id="329">
+  <properties>
+   <property name="Collision" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="332">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="8" width="14" height="7"/>
+   <object id="2" x="3" y="0" width="10" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="333">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="4" width="10" height="11"/>
+  </objectgroup>
+ </tile>
+ <tile id="334">
+  <properties>
+   <property name="Collision" type="int" value="333"/>
+  </properties>
+ </tile>
+ <tile id="335">
+  <properties>
+   <property name="Collision" type="int" value="332"/>
+  </properties>
+ </tile>
+ <tile id="336">
+  <properties>
+   <property name="Collision" type="int" value="333"/>
+  </properties>
+ </tile>
+ <tile id="337">
+  <properties>
+   <property name="Collision" type="int" value="333"/>
+  </properties>
+ </tile>
+ <tile id="338">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="7" width="13" height="9"/>
+  </objectgroup>
+ </tile>
+ <tile id="339">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="7" width="13" height="9"/>
+  </objectgroup>
+ </tile>
+ <tile id="365">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="7" width="13" height="9"/>
+  </objectgroup>
+ </tile>
+ <tile id="366">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0" width="14" height="16"/>
+  </objectgroup>
+ </tile>
  <tile id="379">
   <properties>
    <property name="Collision" type="int" value="55"/>
+  </properties>
+ </tile>
+ <tile id="381">
+  <properties>
+   <property name="Collision" type="int" value="57"/>
+  </properties>
+ </tile>
+ <tile id="382">
+  <properties>
+   <property name="Collision" type="int" value="58"/>
+  </properties>
+ </tile>
+ <tile id="383">
+  <properties>
+   <property name="Collision" type="int" value="59"/>
+  </properties>
+ </tile>
+ <tile id="384">
+  <properties>
+   <property name="Collision" type="int" value="59"/>
+  </properties>
+ </tile>
+ <tile id="386">
+  <properties>
+   <property name="Collision" type="int" value="332"/>
+  </properties>
+ </tile>
+ <tile id="388">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0" width="13" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="389">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6" y="0" width="10" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="390">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="9" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="391">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="0" width="7" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="392">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0" width="14" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="393">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0" width="14" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="437">
+  <properties>
+   <property name="Collision" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="440">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="441">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="0" width="10" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="442">
+  <properties>
+   <property name="Collision" type="int" value="441"/>
+  </properties>
+ </tile>
+ <tile id="443">
+  <properties>
+   <property name="Collision" type="int" value="441"/>
+  </properties>
+ </tile>
+ <tile id="444">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0" width="14" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="445">
+  <properties>
+   <property name="Collision" type="int" value="441"/>
+  </properties>
+ </tile>
+ <tile id="446">
+  <properties>
+   <property name="Collision" type="int" value="441"/>
+  </properties>
+ </tile>
+ <tile id="447">
+  <properties>
+   <property name="Collision" type="int" value="441"/>
   </properties>
  </tile>
  <tile id="487">
@@ -30,9 +342,104 @@
    <property name="Collision" type="int" value="55"/>
   </properties>
  </tile>
+ <tile id="489">
+  <properties>
+   <property name="Collision" type="int" value="57"/>
+  </properties>
+ </tile>
+ <tile id="490">
+  <properties>
+   <property name="Collision" type="int" value="58"/>
+  </properties>
+ </tile>
+ <tile id="491">
+  <properties>
+   <property name="Collision" type="int" value="59"/>
+  </properties>
+ </tile>
+ <tile id="494">
+  <properties>
+   <property name="Collision" type="int" value="440"/>
+  </properties>
+ </tile>
+ <tile id="495">
+  <properties>
+   <property name="Collision" type="int" value="441"/>
+  </properties>
+ </tile>
+ <tile id="545">
+  <properties>
+   <property name="Collision" type="int" value="5"/>
+  </properties>
+ </tile>
  <tile id="595">
   <properties>
    <property name="Collision" type="int" value="55"/>
+  </properties>
+ </tile>
+ <tile id="597">
+  <properties>
+   <property name="Collision" type="int" value="57"/>
+  </properties>
+ </tile>
+ <tile id="598">
+  <properties>
+   <property name="Collision" type="int" value="58"/>
+  </properties>
+ </tile>
+ <tile id="599">
+  <properties>
+   <property name="Collision" type="int" value="59"/>
+  </properties>
+ </tile>
+ <tile id="653">
+  <properties>
+   <property name="Collision" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="683">
+  <properties>
+   <property name="Collision" type="int" value="332"/>
+  </properties>
+ </tile>
+ <tile id="684">
+  <properties>
+   <property name="Collision" type="int" value="333"/>
+  </properties>
+ </tile>
+ <tile id="685">
+  <properties>
+   <property name="Collision" type="int" value="333"/>
+  </properties>
+ </tile>
+ <tile id="686">
+  <properties>
+   <property name="Collision" type="int" value="332"/>
+  </properties>
+ </tile>
+ <tile id="687">
+  <properties>
+   <property name="Collision" type="int" value="333"/>
+  </properties>
+ </tile>
+ <tile id="688">
+  <properties>
+   <property name="Collision" type="int" value="333"/>
+  </properties>
+ </tile>
+ <tile id="689">
+  <properties>
+   <property name="Collision" type="int" value="332"/>
+  </properties>
+ </tile>
+ <tile id="690">
+  <properties>
+   <property name="Collision" type="int" value="333"/>
+  </properties>
+ </tile>
+ <tile id="691">
+  <properties>
+   <property name="Collision" type="int" value="333"/>
   </properties>
  </tile>
  <tile id="703">
@@ -40,9 +447,252 @@
    <property name="Collision" type="int" value="55"/>
   </properties>
  </tile>
+ <tile id="705">
+  <properties>
+   <property name="Collision" type="int" value="57"/>
+  </properties>
+ </tile>
+ <tile id="706">
+  <properties>
+   <property name="Collision" type="int" value="58"/>
+  </properties>
+ </tile>
+ <tile id="707">
+  <properties>
+   <property name="Collision" type="int" value="59"/>
+  </properties>
+ </tile>
+ <tile id="710">
+  <properties>
+   <property name="Collision" type="int" value="332"/>
+  </properties>
+ </tile>
+ <tile id="711">
+  <properties>
+   <property name="Collision" type="int" value="333"/>
+  </properties>
+ </tile>
+ <tile id="712">
+  <properties>
+   <property name="Collision" type="int" value="333"/>
+  </properties>
+ </tile>
+ <tile id="713">
+  <properties>
+   <property name="Collision" type="int" value="332"/>
+  </properties>
+ </tile>
+ <tile id="714">
+  <properties>
+   <property name="Collision" type="int" value="333"/>
+  </properties>
+ </tile>
+ <tile id="715">
+  <properties>
+   <property name="Collision" type="int" value="333"/>
+  </properties>
+ </tile>
+ <tile id="716">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="7" width="13" height="9"/>
+  </objectgroup>
+ </tile>
+ <tile id="717">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="7" width="13" height="9"/>
+  </objectgroup>
+ </tile>
+ <tile id="743">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="7" width="13" height="9"/>
+  </objectgroup>
+ </tile>
+ <tile id="744">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0" width="14" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="761">
+  <properties>
+   <property name="Collision" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="764">
+  <properties>
+   <property name="Collision" type="int" value="332"/>
+  </properties>
+ </tile>
+ <tile id="765">
+  <properties>
+   <property name="Collision" type="int" value="332"/>
+  </properties>
+ </tile>
+ <tile id="766">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0" width="13" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="767">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6" y="0" width="10" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="768">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="9" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="769">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="0" width="7" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="770">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0" width="14" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="771">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0" width="14" height="15"/>
+  </objectgroup>
+ </tile>
  <tile id="811">
   <properties>
    <property name="Collision" type="int" value="55"/>
+  </properties>
+ </tile>
+ <tile id="813">
+  <properties>
+   <property name="Collision" type="int" value="57"/>
+  </properties>
+ </tile>
+ <tile id="814">
+  <properties>
+   <property name="Collision" type="int" value="58"/>
+  </properties>
+ </tile>
+ <tile id="815">
+  <properties>
+   <property name="Collision" type="int" value="59"/>
+  </properties>
+ </tile>
+ <tile id="818">
+  <properties>
+   <property name="Collision" type="int" value="440"/>
+  </properties>
+ </tile>
+ <tile id="822">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0" width="14" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="919">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0" width="15" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="921">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="11" y="4" width="5" height="11"/>
+   <object id="2" x="4" y="0" width="12" height="4"/>
+  </objectgroup>
+ </tile>
+ <tile id="922">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="4" width="5" height="11"/>
+   <object id="2" x="0" y="0" width="13" height="4"/>
+  </objectgroup>
+ </tile>
+ <tile id="923">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="5" width="8" height="10"/>
+   <object id="2" x="0" y="0" width="16" height="5"/>
+  </objectgroup>
+ </tile>
+ <tile id="924">
+  <properties>
+   <property name="Collision" type="int" value="921"/>
+  </properties>
+ </tile>
+ <tile id="925">
+  <properties>
+   <property name="Collision" type="int" value="923"/>
+  </properties>
+ </tile>
+ <tile id="926">
+  <properties>
+   <property name="Collision" type="int" value="923"/>
+  </properties>
+ </tile>
+ <tile id="1027">
+  <properties>
+   <property name="Collision" type="int" value="919"/>
+  </properties>
+ </tile>
+ <tile id="1029">
+  <properties>
+   <property name="Collision" type="int" value="60"/>
+  </properties>
+ </tile>
+ <tile id="1030">
+  <properties>
+   <property name="Collision" type="int" value="61"/>
+  </properties>
+ </tile>
+ <tile id="1031">
+  <properties>
+   <property name="Collision" type="int" value="60"/>
+  </properties>
+ </tile>
+ <tile id="1032">
+  <properties>
+   <property name="Collision" type="int" value="61"/>
+  </properties>
+ </tile>
+ <tile id="1033">
+  <properties>
+   <property name="Collision" type="int" value="59"/>
+  </properties>
+ </tile>
+ <tile id="1034">
+  <properties>
+   <property name="Collision" type="int" value="59"/>
+  </properties>
+ </tile>
+ <tile id="1135">
+  <properties>
+   <property name="Collision" type="int" value="919"/>
+  </properties>
+ </tile>
+ <tile id="1137">
+  <properties>
+   <property name="Collision" type="int" value="60"/>
+  </properties>
+ </tile>
+ <tile id="1138">
+  <properties>
+   <property name="Collision" type="int" value="61"/>
+  </properties>
+ </tile>
+ <tile id="1139">
+  <properties>
+   <property name="Collision" type="int" value="60"/>
+  </properties>
+ </tile>
+ <tile id="1140">
+  <properties>
+   <property name="Collision" type="int" value="61"/>
+  </properties>
+ </tile>
+ <tile id="1141">
+  <properties>
+   <property name="Collision" type="int" value="59"/>
+  </properties>
+ </tile>
+ <tile id="1142">
+  <properties>
+   <property name="Collision" type="int" value="59"/>
   </properties>
  </tile>
 </tileset>
